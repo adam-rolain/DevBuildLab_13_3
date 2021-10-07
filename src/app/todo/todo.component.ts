422c43f300cea7d0847461c45aa7d482b90628ef
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-10-07 15:17:57 -0400

    Comleted todo list app with ability to check off items from list and add new items

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -17,9 +17,23 @@ export class TodoComponent implements OnInit {
     { task: 'Mow the lawn', completed: false },
   ];
 
+  newTask: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  completeTask(i: number): void {
+    this.todos[i].completed = true;
+  }
+
+  addTask(): void {
+    this.todos.push({ task: this.newTask, completed: false });
+  }
+
+  removeTask(i: number): void {
+    this.todos.splice(i, 1);
+  }
 }
